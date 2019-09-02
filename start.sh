b8fe262e0c4d9ddfdcfe477ca877dddd51aa8890
Leonardo <leonardosabinoti@gmail.com>
2019-09-01 21:29:06 -0300

    Modificações Sh 2

--- start.sh
+++ start.sh
@@ -9,3 +9,19 @@ sleep 2;
 docker run --rm -v $(pwd):/app composer install
 
 docker-compose up --build
+
+docker-compose exec db bash
+
+mysql -u root -p
+
+show databases;
+
+GRANT ALL ON sod.* TO 'sistemas'@'%' IDENTIFIED BY '123456';
+
+FLUSH PRIVILEGES;
+
+EXIT;
+
+exit
+
+docker-compose exec app php artisan migrate
