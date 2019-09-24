ed22d900f7ce5ed45690b2cdc06b4c1320798688
Leonardo Costa Sabino <leonardosabinoti@gmail.com>
2019-09-24 09:39:56 -0300

    Configuração do ambiente

--- start.sh
+++ start.sh
@@ -1,16 +1,23 @@
 #bin/bash
 
-echo -e "Bem ao Docker-Laravel\n";
+echo -e "Bem-vindo ao SOD 2.0\n";
 
 echo "Criado pela DigitalOcean, desenvolvedor utilizador: Leonardo Costa Sabino";
 
-sleep 2;
+cp .env.example .env
 
 docker run --rm -v $(pwd):/app composer install
 
-sudo chmod 777 -R ../Docker-Laravel
+docker run -v $(pwd):/app -w /app node:7 npm install
+
+sudo chmod 777 -R ../Docker-Laravel/storage
+
+docker-compose kill
 
 docker-compose up -d --build
-echo -e 'docker-compose exec db mysql -u root -p\n docker-compose exec db show databases;\n docker-compose exec db GRANT ALL ON sod.* TO 'sistemas'@'%' IDENTIFIED BY '123456';\n docker-compose exec db FLUSH PRIVILEGES;'
+
+docker-compose exec app php artisan storage:link
+
+#echo -e 'docker-compose exec db mysql -u root -p\n docker-compose exec db show databases;\n docker-compose exec db GRANT ALL ON sod.* TO 'sistemas'@'%' IDENTIFIED BY '123456';\n docker-compose exec db FLUSH PRIVILEGES;'
 
 #docker-compose exec app php artisan migrate
