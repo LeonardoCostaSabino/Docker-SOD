9764640fab410c4542b0648f2f8269646dde66bb
Leonardo Costa Sabino <leonardosabinoti@gmail.com>
2019-09-25 18:05:03 -0300

    Layout

--- resources/views/public/head.blade.php
+++ resources/views/public/head.blade.php
@@ -1,8 +1,27 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Sod Unisuam</title>
+<head>
+<html lang="pt-br">
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta charset="utf-8">
 
-<link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css"/>
+    <title>Carousel Template for Bootstrap</title>
 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<style type="text/css">
+  #captain
+      {
+                      height: 45px;
+                      margin-top: 10px; 
+      }
+  </style>
+
+    <!-- estilo do carousel para as imagens ficarem com um bom tamanho -->
+    <link href="./css/carousel.css" rel="stylesheet">
+  </head>
--- resources/views/public/header.blade.php
+++ resources/views/public/header.blade.php
@@ -1,4 +1,29 @@
-<nav class="navbar navbar-expand-lg bg-gradient-primary">
+<header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-info">
+        <a class="navbar-brand" href="#"><img src="images/logo1.png" id="captain"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="login">Entrar<span class="sr-only">(Página atual)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="funcoes">Funcionalidades do SOD</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="sobre">Sobre nós</a>
+            </li>
+            <span class="font-italic navbar-text ml-5 text-info">
+                SOD o Sistema de Otimização de Dados da Unisuam que foi para voce!
+            </span>
+        </ul>
+    </div>
+      </nav>
+    </header>
+
+<!-- <nav class="navbar navbar-expand-lg bg-gradient-primary">
     <a class="navbar-brand" href="/login">SOD-UNISUAM</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
         <span class="navbar-toggler-icon"></span>
@@ -19,4 +44,4 @@
             </span>
         </ul>
     </div>
-</nav>
+</nav> -->
