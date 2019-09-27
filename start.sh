605b77e3e3240f629a090c3105bb2c38becc0d61
Sod-Unisuam <S1st3m@SOD>
2019-09-27 14:54:50 -0300

    Atualização do start.sh

--- start.sh
+++ start.sh
@@ -5,34 +5,38 @@ echo "Bem-vindo ao SOD 2.0\n";
 echo "Criado pela DigitalOcean, desenvolvedor utilizador: Leonardo Costa Sabino";
 
 echo "-------------MENU------------------
-      | 1- Iniciar atualização do sistema"
+      | 1 - Iniciar atualização do sistema
+      | 2 - Atualizar o .env
+      | 3 - Adicionar Host no Linux
+      | 4 - Atualizar o ambiente"
 
 read -r opcao
 
-cp .env.example .env
-
-cp .env.example .env.backup
-
-docker run --rm -v $(pwd):/app composer install
-
-docker run -v $(pwd):/app -w /app node:7 npm install
-
-docker-compose kill
-
-docker-compose up -d --build
-
-sudo chmod 777 -R .
-
-docker-compose exec app php artisan storage:link
-
-docker-compose exec app php artisan key:generate
-
-sudo echo "127.0.1.1 local.sod.unisuam.edu.br" >> /etc/hosts
-
-echo "Finalizado"
-
-echo "Por favor entre no seu navegador e digite: local.sod.unisuam.edu.br/home"
-
+  case $opcao in
+    1)
+      docker run --rm -v $(pwd):/app composer install
+      docker run -v $(pwd):/app -w /app node:7 npm install
+      docker-compose kill
+      sudo chmod 777 -R .
+      docker-compose up -d
+      echo "Finalizado"
+      echo "Por favor entre no seu navegador e digite: local.sod.unisuam.edu.br/home"
+    ;;
+    2)
+      cp .env.backup .env
+      exit;
+    ;;
+    3)
+      sudo echo "127.0.1.1 local.sod.unisuam.edu.br" >> /etc/hosts
+    ;;
+    4)
+      docker-compose up -d --build
+      docker-compose exec app php artisan storage:link
+      docker-compose exec app php artisan key:generate
+      echo "Finalizado"
+      echo "Por favor entre no seu navegador e digite: local.sod.unisuam.edu.br/home"
+      ;;
+esac
 #echo -e 'docker-compose exec db mysql -u root -p\n docker-compose exec db show databases;\n docker-compose exec db GRANT ALL ON sod.* TO 'sistemas'@'%' IDENTIFIED BY '123456';\n docker-compose exec db FLUSH PRIVILEGES;'
 
 #docker-compose exec app php artisan migrate
