0756bfa2538274bd191d371a97cbae0ec71352fa
Leonardo Costa Sabino <leonardosabinoti@gmail.com>
2019-10-02 11:57:03 -0300

    Atualização de Scripts

--- start.sh
+++ start.sh
@@ -5,18 +5,18 @@ echo "Bem-vindo ao SOD 2.0\n";
 echo "Criado pela DigitalOcean, desenvolvedor utilizador: Leonardo Costa Sabino";
 
 echo "-------------MENU------------------
-      | 1 - Iniciar atualização do sistema
+      | 1 - Atualizar o ambiente
       | 2 - Atualizar o .env
       | 3 - Adicionar Host no Linux
       | 4 - Atualizar o ambiente
-      | 5 - Adicionar Permissões ao projeto"
+      | 5 - Primeira instalação do SOD 2.0"
 
 read -r opcao
 
   case $opcao in
     1)
       docker run --rm -v $(pwd):/app composer install
-      docker run -v $(pwd):/app -w /app node:latest npm install
+      docker run --rm -v $(pwd):/app -w /app node:latest npm install
       docker-compose kill
       sudo chmod 777 -R .
       docker-compose up -d
@@ -38,9 +38,17 @@ read -r opcao
       echo "Por favor entre no seu navegador e digite: local.sod.unisuam.edu.br/home"
       ;;
     5)
-
-
-    ;;
+      docker run --rm -v $(pwd):/app composer install
+      docker run --rm -v $(pwd):/app -w /app node:latest npm install
+      docker-compose kill
+      sudo chmod 777 -R .
+      docker-compose up -d
+      docker-compose exec app php artisan storage:link
+      docker-compose exec app php artisan key:generate
+      sudo echo "127.0.1.1 local.sod.unisuam.edu.br" >> /etc/hosts
+      echo "Finalizado"
+      echo "Por favor entre no seu navegador e digite: local.sod.unisuam.edu.br/home"
+      ;;
 esac
 #echo -e 'docker-compose exec db mysql -u root -p\n docker-compose exec db show databases;\n docker-compose exec db GRANT ALL ON sod.* TO 'sistemas'@'%' IDENTIFIED BY '123456';\n docker-compose exec db FLUSH PRIVILEGES;'
 
